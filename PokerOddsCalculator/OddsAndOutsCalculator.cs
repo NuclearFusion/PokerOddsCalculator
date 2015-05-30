960678ddce920f7c159f2ebc426602fb8dad8c88
NuclearFusion <raivis101@inbox.lv>
2015-05-30 21:23:06 +0300

    Finished TwoPair() and ThreeOfAKind()

--- PokerOddsCalculator/OddsAndOutsCalculator.cs
+++ PokerOddsCalculator/OddsAndOutsCalculator.cs
@@ -61,9 +61,11 @@ namespace PokerOddsCalculator
 
             OnePair();
             TwoPair();
+            ThreeOfAKind();
 
             Console.WriteLine(Result.OnePair);
             Console.WriteLine(Result.TwoPair);
+            Console.WriteLine(Result.ThreeOfAKind);
         }
 
         public void RoyalFlush()
@@ -140,14 +142,98 @@ namespace PokerOddsCalculator
 
         public void ThreeOfAKind()
         {
-
+            int pCard1Match = 0;
+            int pCard2Match = 0;
+            switch (combinationCardCount)
+            {
+                case 2:
+                    Console.WriteLine("ThreeOfAKind - Case 2: Initiated");
+                    if (Combination[0].Rank == Combination[1].Rank)
+                    {
+                        flopCardRoll = (2.0 / 50.0) * (48.0 / 49.0) * (47.0 / 48.0) * (46.0 / 47.0) * (45.0 / 46.0);
+                        Result.ThreeOfAKind = Math.Round((flopCardRoll * 5) * 100, 1);
+                    }
+                    else
+                    {
+                        flopCardRoll = (6.0 / 50.0) * (2.0 / 49.0) * (47.0 / 48.0) * (46.0 / 47.0) * (45.0 / 46.0);
+                        Result.ThreeOfAKind = Math.Round((flopCardRoll * 10) * 100, 1);
+                    }
+                    break;
+                case 5:
+                    Console.WriteLine("ThreeOfAKind - Case 5: Initiated");
+                    for (int i = 2; i < combinationCardCount; i++)
+                    {
+                        if (Combination[0].Rank == Combination[1].Rank)
+                        {
+                            pCard1Match++;
+                            pCard2Match++;
+                        }
+                        if (Combination[0].Rank == Combination[i].Rank)
+                        {
+                            pCard1Match++;
+                        }
+                        if (Combination[1].Rank == Combination[i].Rank)
+                        {
+                            pCard2Match++;
+                        }
+                    }
+                    if (pCard1Match > 1 || pCard2Match > 1)
+                    {
+                        Result.ThreeOfAKind = 100;
+                    }
+                    else if (pCard1Match == 1 || pCard2Match == 1)
+                    {
+                        turnRoll = (2.0 / 47.0) * (45.0 / 46.0);
+                        Result.ThreeOfAKind = Math.Round((turnRoll * 1) * 100, 1);
+                    }
+                    else
+                    {
+                        turnRoll = (3.0 / 47.0) * (2.0 / 46.0);
+                        Result.ThreeOfAKind = Math.Round((turnRoll * 1) * 100, 1);
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("ThreeOfAKind - Case 6: Initiated");
+                    for (int i = 2; i < combinationCardCount; i++)
+                    {
+                        if (Combination[0].Rank == Combination[1].Rank)
+                        {
+                            pCard1Match++;
+                            pCard2Match++;
+                        }
+                        if (Combination[0].Rank == Combination[i].Rank)
+                        {
+                            pCard1Match++;
+                        }
+                        if (Combination[1].Rank == Combination[i].Rank)
+                        {
+                            pCard2Match++;
+                        }
+                    }
+                    if (pCard1Match > 1 || pCard2Match > 1)
+                    {
+                        Result.ThreeOfAKind = 100;
+                    }
+                    else if (pCard1Match == 1 || pCard2Match == 1)
+                    {
+                        riverRoll = (2.0 / 46.0);
+                        Result.ThreeOfAKind = Math.Round((riverRoll * 1) * 100, 1);
+                    }
+                    else
+                    {
+                        Result.ThreeOfAKind = 0;
+                    }
+                    break;
+            }
         }
 
         public void TwoPair()
         {
+            bool onePair = false;
             switch (combinationCardCount)
             {
                 case 2:
+                    Console.WriteLine("TwoPair - Case 2: Initiated");
                     if (Combination[0].Rank == Combination[1].Rank)
                     {
                         flopCardRoll = (48.0 / 50.0) * (3.0 / 49.0) * (44.0 / 48.0) * (43.0 / 47.0) * (42.0 / 46.0);
@@ -160,6 +246,7 @@ namespace PokerOddsCalculator
                     }
                     break;
                 case 5:
+                    Console.WriteLine("TwoPair - Case 5: Initiated");
                     if (Combination[0].Rank == Combination[1].Rank)
                     {
                         for (int i = 2; i < combinationCardCount; i++)
@@ -175,6 +262,11 @@ namespace PokerOddsCalculator
                                 }
                             }
                         }
+                        if (Result.TwoPair != 100)
+                        {
+                            turnRoll = (45.0 / 47.0) * (3.0 / 46.0);
+                            Result.TwoPair = Math.Round((turnRoll * 1) * 100, 1);
+                        }
                     }
                     else
                     {
@@ -189,32 +281,93 @@ namespace PokerOddsCalculator
                                         Result.TwoPair = 100;
                                     }
                                 }
+                                onePair = true;
                             }
+                        }
+                        if (Result.TwoPair != 100 && onePair == true)
+                        {
+                            turnRoll = (45.0 / 47.0) * (3.0 / 46.0);
+                            Result.TwoPair = Math.Round((turnRoll * 1) * 100, 1);
+                        }
+                        if (Result.TwoPair != 100 && onePair == false)
+                        {
+                            turnRoll = (6.0 / 47.0) * (3.0 / 46.0);
+                            Result.TwoPair = Math.Round((turnRoll * 1) * 100, 1);
                         }
                     }
                     break;
                 case 6:
-
+                    Console.WriteLine("TwoPair - Case 6: Initiated");
+                    if (Combination[0].Rank == Combination[1].Rank)
+                    {
+                        for (int i = 2; i < combinationCardCount; i++)
+                        {
+                            for (int j = 2; j < combinationCardCount; j++)
+                            {
+                                if (i != j)
+                                {
+                                    if (Combination[i].Rank == Combination[j].Rank)
+                                    {
+                                        Result.TwoPair = 100;
+                                    }
+                                }
+                            }
+                        }
+                        if (Result.TwoPair != 100)
+                        {
+                            riverRoll = (3.0 / 46.0);
+                            Result.TwoPair = Math.Round((riverRoll * 1) * 100, 1);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 2; i < combinationCardCount; i++)
+                        {
+                            if (Combination[0].Rank == Combination[i].Rank)
+                            {
+                                for (int j = 2; j < combinationCardCount; j++)
+                                {
+                                    if (Combination[1].Rank == Combination[j].Rank)
+                                    {
+                                        Result.TwoPair = 100;
+                                    }
+                                }
+                                onePair = true;
+                            }
+                        }
+                        if (Result.TwoPair != 100 && onePair == true)
+                        {
+                            riverRoll = (3.0 / 46.0);
+                            Result.TwoPair = Math.Round((riverRoll * 1) * 100, 1);
+                        }
+                        if (Result.TwoPair != 100 && onePair == false)
+                        {
+                            Result.TwoPair = 0;
+                        }
+                    }
                     break;
             }
         }
 
         public void OnePair()
         {
+            Console.WriteLine("OnePair: Initiated");
             switch (combinationCardCount)
             {
                 case 2:
+                    Console.WriteLine("OnePair - Case 2: Initiated");
                     if (Combination[0].Rank == Combination[1].Rank)
                     {
                         Result.OnePair = 100;
                     }
                     else
                     {
-                        flopCardRoll = (6.0 / 50.0) * (45.0 / 49.0) * (44.0 / 48.0) * (43.0 / 47.0) * (42.0 / 46.0);
+                        flopCardRoll = (6.0 / 50.0) * (44.0 / 49.0) * (43.0 / 48.0) * (42.0 / 47.0) * (41.0 / 46.0);
                         Result.OnePair = Math.Round((flopCardRoll * 5) * 100, 1);
                     }
                     break;
                 case 5:
+                    Console.WriteLine("OnePair - Case 5: Initiated");
                     for (int i = 0; i < combinationCardCount; i++)
                     {
                         for (int j = 0; j < combinationCardCount; j++)
@@ -224,17 +377,18 @@ namespace PokerOddsCalculator
                                 if (Combination[i].Rank == Combination[j].Rank)
                                 {
                                     Result.OnePair = 100;
-                                }
-                                else
-                                {
-                                    turnRoll = (6.0 / 50.0) * (45.0 / 49.0);
-                                    Result.OnePair = Math.Round((turnRoll * 2) * 100, 1);
                                 }
                             }
                         }
                     }
+                    if (Result.OnePair != 100)
+                    {
+                        turnRoll = (6.0 / 47.0) * (41.0 / 46.0);
+                        Result.OnePair = Math.Round((turnRoll * 2) * 100, 1);
+                    }
                     break;
                 case 6:
+                    Console.WriteLine("OnePair - Case 6: Initiated");
                     for (int i = 0; i < combinationCardCount; i++)
                     {
                         for (int j = 0; j < combinationCardCount; j++)
@@ -245,13 +399,13 @@ namespace PokerOddsCalculator
                                 {
                                     Result.OnePair = 100;
                                 }
-                                else
-                                {
-                                    riverRoll = (6.0 / 50.0);
-                                    Result.OnePair = Math.Round((riverRoll * 1) * 100, 1);
-                                }
                             }
                         }
+                    }
+                    if (Result.OnePair != 100)
+                    {
+                        riverRoll = (6.0 / 46.0);
+                        Result.OnePair = Math.Round((riverRoll * 1) * 100, 1);
                     }
                     break;
             }
